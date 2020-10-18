ec60de65752b05adb8f78ceeebdc94dc3bd15e67
damodarams <damodaram1991@gmail.com>
2020-10-18 23:46:04 +0530

    Updates

---
+++ precache-manifest.9a7b831b5f0cb785811809633f572e23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6918c4e2c06c82a9f3c913fe4bf942bc",
+    "url": "/sapspacex/index.html"
+  },
+  {
+    "revision": "703e44f3beca11654165",
+    "url": "/sapspacex/static/css/main.55bee1e8.chunk.css"
+  },
+  {
+    "revision": "3bf6f691c92c1617d5b0",
+    "url": "/sapspacex/static/js/2.74f09717.chunk.js"
+  },
+  {
+    "revision": "12db2d529bf89ade53c7acc352992b69",
+    "url": "/sapspacex/static/js/2.74f09717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703e44f3beca11654165",
+    "url": "/sapspacex/static/js/main.e55989fe.chunk.js"
+  },
+  {
+    "revision": "893f1076c5aede629e26",
+    "url": "/sapspacex/static/js/runtime-main.ab4ca294.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sapspacex/static/media/logo.5d5d9eef.svg"
+  }
+]);
